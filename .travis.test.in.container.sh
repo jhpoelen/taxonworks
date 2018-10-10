0ef92e1fd3578b9f679a24a8949b13d7782241de
jhpoelen <jhpoelen@xs4all.nl>
2018-10-10 14:20:25 -0700

    set workdir for container run; remove duplicate npm/bundler installes

--- .travis.test.in.container.sh
+++ .travis.test.in.container.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -x
 set -e
-nvm install node
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.7.0
 export PATH=$HOME/.yarn/bin:$PATH
-npm install
-bundle install --without development production
 "export DISPLAY=:99.0"
 "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1600x1200x16"
 sleep 3
