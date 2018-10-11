8563273ebd16f42568eaa466f54fe4ca35612eab
jhpoelen <jhpoelen@xs4all.nl>
2018-10-11 14:48:05 -0700

    towards testing in docker-compose

--- .travis.build.image.sh
+++ .travis.build.image.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker build -f Dockerfile.base . --tag ${TRAVIS_REPO_SLUG}:base
 docker build -f Dockerfile . --tag ${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}
--- .travis.test.in.container.sh
+++ .travis.test.in.container.sh
@@ -5,13 +5,22 @@ set -e
 #export DISPLAY=:99.0
 #/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1600x1200x16
 #sleep 3
-cp config/database.yml.travis config/database.yml
-cp config/secrets.yml.example config/secrets.yml
+cat config/database.yml
+cat config/secrets.yml
+cat Gemfile.lock
+
 mkdir -p tmp/
 mkdir -p tmp/downloads
-bundle exec rake db:create RAILS_ENV=test # database user by default is `travis` 
-bundle exec rake db:migrate RAILS_ENV=test
+export RAILS_ENV=test
+bundle exec rake db:create 
+bundle exec rake db:migrate
+npm install
+# make sure to install test dependencies
+# see https://stackoverflow.com/a/4143287
+rm .bundle/config
+bundle install --without development production
+npm run webpack-test
 ls -alh bin 
 ls -alh public/packs-test 
 bundle exec rake assets:precompile
-bundle exec rspec
+bundle exec rspec --format documentation
